d4866915e1347cc99734cbc29a4e8c778724910b
Kkuil <3024067144@qq.com>
2023-08-08 11:58:13 +0800

    add:utils

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,26 @@ import HelloWorld from "@/components/HelloWorld.vue"
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    >
+    <header>
+        <img
+            alt="Vue logo"
+            class="logo"
+            src="@/assets/logo.svg"
+            width="125"
+            height="125"
+        />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="wrapper">
+            <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">
-          Home
-        </RouterLink>
-        <RouterLink to="/about">
-          About
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
+            <nav>
+                <RouterLink to="/"> Home </RouterLink>
+                <RouterLink to="/about"> About </RouterLink>
+            </nav>
+        </div>
+    </header>
 
-  <RouterView />
+    <RouterView />
 </template>
 
 <style scoped>
