1e099cac025afd7f9b26b5e5a67ecfb5e35bb042
Kkuil <3024067144@qq.com>
2024-05-22 16:53:26 +0800

    fix: 修复模板

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue"
-</script>
+<script lang="ts" setup></script>
 
 <template>
-    <main>
-        <TheWelcome />
-    </main>
+    <main>Hello World</main>
 </template>
