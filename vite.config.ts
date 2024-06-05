4bc5b33be16f38c5d17918717cc3cb1e3cda1321
Kkuil <3024067144@qq.com>
2024-06-05 20:14:08 +0800

    feat: 新增element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import {fileURLToPath, URL} from "node:url"
+import { fileURLToPath, URL } from "node:url"
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
-import {ElementPlusResolver} from "unplugin-vue-components/resolvers"
-import {defineConfig} from "vite"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
